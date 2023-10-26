48fa695b757962d79fa4806e1a76e40395af4ab7
kruschewskyjoao <joaokruschewsky@gmail.com>
2023-10-26 11:56:39 -0300

    valida publicacao antes de criar

--- APP/src/modelos/publicacao.go
+++ APP/src/modelos/publicacao.go
@@ -1,6 +1,10 @@
 package modelos
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
 type Publicacao struct {
 	ID        uint64    `json:"id,omitempty"`
@@ -10,4 +14,29 @@ type Publicacao struct {
 	AutorNick uint64    `json:"autorNick,omitempty"`
 	Curtidas  uint64    `json:"curtidas"`
 	CreatedAt time.Time `json:"createdAt,omitempty"`
+}
+
+// Preparar vai chamar os métodos para validar e formatar a publicação recebida
+func (publicacao *Publicacao) Preparar() error {
+	if erro := publicacao.validar(); erro != nil {
+		return erro
+	}
+
+	publicacao.formatar()
+	return nil
+}
+
+func (publicacao *Publicacao) validar() error {
+	if publicacao.Titulo == "" {
+		return errors.New("O titulo é obrigatorio")
+	}
+	if publicacao.Conteudo == "" {
+		return errors.New("O Conteudo é obrigatorio")
+	}
+	return nil
+}
+
+func (publicacao *Publicacao) formatar() {
+	publicacao.Titulo = strings.TrimSpace(publicacao.Titulo)
+	publicacao.Conteudo = strings.TrimSpace(publicacao.Conteudo)
 }
