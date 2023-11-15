1b1cb7899a80fab1f6437e7629b13980710a2dc7
joaokruschewsky <joaokruschewsky@gmail.com>
2023-11-15 12:14:01 -0300

    remove webapp

--- webAPP/go.mod
+++
@@ -1,8 +0,0 @@
-module webapp
-
-go 1.21.3
-
-require (
-	github.com/gorilla/mux v1.8.0 // indirect
-	github.com/gorilla/securecookie v1.1.2 // indirect
-)
--- webAPP/src/modelos/DadosAutenticacao.go
+++
@@ -1,7 +0,0 @@
-package modelos
-
-// DadosAutenticacao contem o id e token do usuario
-type DadosAutenticacao struct {
-	ID    string `json:"id"`
-	Token string `json:"token"`
-}
